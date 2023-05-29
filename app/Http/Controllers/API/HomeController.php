9d307237c68861518295d4a1f0058c149bc6ba03
Asmaa Marzouk <fsd531758@gmail.com>
2023-05-29 14:24:14 +0300

    add is trending products to home api

--- app/Http/Controllers/API/HomeController.php
+++ app/Http/Controllers/API/HomeController.php
@@ -6,11 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\AboutResource;
 use App\Http\Resources\ExperienceResource;
 use App\Http\Resources\PartnerResource;
+use App\Http\Resources\ProductResource;
 use App\Http\Resources\ServiceResource;
 use App\Http\Resources\SliderResource;
 use App\Http\Resources\SummaryResource;
+use App\Http\Resources\TrendingProductsResource;
 use App\Models\Page;
 use App\Models\Partner;
+use App\Models\Product;
 use App\Models\Service;
 use App\Models\Slider;
 use Illuminate\Http\Request;
@@ -27,16 +30,18 @@ class HomeController extends Controller
             $services             = Service ::active()->get();
             $experience           = Page::where('identifier', 'experience')->first();
             $partners             = Partner::active()->get();
+            $trending_products    = Product::active()->isTrending()->get();
 
 
 
             return successResponse(
                 [
-                    "sliders"            => SliderResource::collection($sliders),
-                    "about_us"           => new AboutResource($about),
-                    "services"           => ServiceResource::collection($services),
-                    "experience"         => new ExperienceResource($experience),
-                    "partners"           => PartnerResource::collection($partners),
+                    "sliders"                => SliderResource::collection($sliders),
+                    "about_us"               => new AboutResource($about),
+                    "services"               => ServiceResource::collection($services),
+                    "experience"             => new ExperienceResource($experience),
+                    "partners"               => PartnerResource::collection($partners),
+                    "trending_products"      => ProductResource::collection($trending_products),
                 ],
                 trans("message.retrieved_successfully"),
                 200
