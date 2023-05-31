c1decf203b9cf8f79cafde1abb808e508c30b5c6
Asmaa Marzouk <fsd531758@gmail.com>
2023-05-31 15:21:19 +0300

    Add Poducts/Categories/Section/ContactRequest/QuoteRequest Counts To Admin Panel

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -29,13 +29,39 @@
             </div>
             @endpermission
 
-            @permission('read-projects')
+      
+            
+            @permission('read-sections')
             <div class="col-xl-4">
                 <div class="card card-custom card-stretch gutter-b wave wave-primary wave-animate-slow">
                     <div class="card-body">
-                        <i class="fas fa-project-diagram  fa-3x"></i>
-                        <span class="card-title font-weight-bolder text-dark-75 font-size-h1 mb-0 mt-6 d-block">{{ \App\Models\Project::count() }}</span>
-                        <span class="font-weight-bold font-size-sm"><a href="{{route('projects.index')}}">{{__('words.projects')}}</a></span>
+                        <i class="fas fa-layer-group  fa-3x"></i>
+                        <span class="card-title font-weight-bolder text-dark-75 font-size-h1 mb-0 mt-6 d-block">{{ \App\Models\Section::count() }}</span>
+                        <span class="font-weight-bold font-size-sm"><a href="{{route('sections.index')}}">{{__('words.sections')}}</a></span>
+                    </div>
+                </div>
+            </div>
+            @endpermission
+          
+            @permission('read-categories')
+            <div class="col-xl-4">
+                <div class="card card-custom card-stretch gutter-b wave wave-primary wave-animate-slow">
+                    <div class="card-body">
+                        <i class="far fa-lightbulb  fa-3x"></i>
+                        <span class="card-title font-weight-bolder text-dark-75 font-size-h1 mb-0 mt-6 d-block">{{ \App\Models\Category::count() }}</span>
+                        <span class="font-weight-bold font-size-sm"><a href="{{route('categories.index')}}">{{__('words.categories')}}</a></span>
+                    </div>
+                </div>
+            </div>
+            @endpermission
+
+            @permission('read-products')
+            <div class="col-xl-4">
+                <div class="card card-custom card-stretch gutter-b wave wave-primary wave-animate-slow">
+                    <div class="card-body">
+                        <i class="fas fa-chair  fa-3x"></i>
+                        <span class="card-title font-weight-bolder text-dark-75 font-size-h1 mb-0 mt-6 d-block">{{ \App\Models\Product::count() }}</span>
+                        <span class="font-weight-bold font-size-sm"><a href="{{route('products.index')}}">{{__('words.products')}}</a></span>
                     </div>
                 </div>
             </div>
@@ -48,6 +74,30 @@
                         <i class="fab fa-servicestack fa-3x"></i>
                         <span class="card-title font-weight-bolder text-dark-75 font-size-h1 mb-0 mt-6 d-block">{{ \App\Models\Service::count() }}</span>
                         <span class="font-weight-bold font-size-sm"><a href="{{route('services.index')}}">{{__('words.services')}}</a></span>
+                    </div>
+                </div>
+            </div>
+            @endpermission
+            
+            @permission('read-contact_requests')
+            <div class="col-xl-4">
+                <div class="card card-custom card-stretch gutter-b wave wave-primary wave-animate-slow">
+                    <div class="card-body">
+                        <i class="fas fa-comment-alt fa-3x"></i>
+                        <span class="card-title font-weight-bolder text-dark-75 font-size-h1 mb-0 mt-6 d-block">{{ \App\Models\ContactUs::count() }}</span>
+                        <span class="font-weight-bold font-size-sm"><a href="{{route('contact_requests.index')}}">{{__('words.contact_requests')}}</a></span>
+                    </div>
+                </div>
+            </div>
+            @endpermission
+            
+            @permission('read-quote_requests')
+            <div class="col-xl-4">
+                <div class="card card-custom card-stretch gutter-b wave wave-primary wave-animate-slow">
+                    <div class="card-body">
+                        <i class="far fa-list-alt fa-3x"></i>
+                        <span class="card-title font-weight-bolder text-dark-75 font-size-h1 mb-0 mt-6 d-block">{{ \App\Models\Quote::count() }}</span>
+                        <span class="font-weight-bold font-size-sm"><a href="{{route('quote_requests.index')}}">{{__('words.quote_requests')}}</a></span>
                     </div>
                 </div>
             </div>
