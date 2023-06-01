e7090e35fc51ee95bef3562c9b1440b8010264d5
Asmaa Marzouk <fsd531758@gmail.com>
2023-06-01 10:57:50 +0300

    Edits : Make Description Required in Services

--- app/Http/Requests/Dashboard/ServiceRequest.php
+++ app/Http/Requests/Dashboard/ServiceRequest.php
@@ -16,12 +16,12 @@ class ServiceRequest extends FormRequest
     {
         $rules = [
             // 'image' => 'required_without:id|max:900|image',
-            'icon' => 'required_without:id|string',
+            'icon' => 'nullable|string',
         ];
         foreach (config('translatable.locales') as $locale) {
             $rules += [$locale . '.title' => ['required', 'string', Rule::unique('service_translations', 'title')->ignore($this->id, 'service_id')]];
-            $rules += [$locale . '.sub_description' => ['nullable', 'string','max:200']];
-            $rules += [$locale . '.description' => ['nullable', 'string']];
+            // $rules += [$locale . '.sub_description' => ['nullable', 'string','max:200']];
+            $rules += [$locale . '.description' => ['required', 'string']];
         }
 
         return $rules;
